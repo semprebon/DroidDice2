d77e45e9524e0d3626d990c7a57a24a7f94470e2
unknown <andrew@.(none)>
2012-03-01 19:09:28 -0500

    Story 9-22 - Data Conversion

--- src/com/droiddice/datastore/DatabaseBuilder.scala
+++ src/com/droiddice/datastore/DatabaseBuilder.scala
@@ -4,10 +4,12 @@ import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 import android.util.Log
+import android.database.Cursor
+import android.content.ContentValues
 
 object DatabaseBuilder {
-	val DATABASE_NAME = "dice"
-    val VERSION = 3
+	val DATABASE_NAME = "data"
+    val VERSION = 6
 }
 
 class DatabaseBuilder(context: Context)
@@ -16,9 +18,7 @@ class DatabaseBuilder(context: Context)
     val TAG = "DatabaseBuilder"
     
     val MIGRATIONS = Map[Int, Migration](
-            1 -> new CreateDiceSetTable,
-            2 -> new CreateNameIndex,
-            3 -> new makeNameNullable
+            6 -> new CreateDiceSetTable
     	)
     	
     def open(): SQLiteDatabase = getWritableDatabase()
@@ -32,13 +32,13 @@ class DatabaseBuilder(context: Context)
     
     override def onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
         for (version <- (oldVersion+1) to newVersion) {
-            val migration = MIGRATIONS(version)
-            if (migration != None) {
-                Log.d(TAG, "Runnning up migration " + version)
-                migration.up(db)
+            MIGRATIONS.get(version).foreach {
+            	Log.d(TAG, "Runnning up migration " + version)
+            	_.up(db)
             }
         }
     }
+
     override def onDowngrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
         for (version <- oldVersion to (newVersion+1) by -1) {
             val migration = MIGRATIONS(version)
@@ -52,60 +52,98 @@ class DatabaseBuilder(context: Context)
 	abstract class Migration {
 	    def up(db: SQLiteDatabase)
 	    def down(db: SQLiteDatabase)
+	    
+	    def eachRow(cursor: Cursor, visitor: (Cursor) => Any) {
+	        cursor.moveToFirst()
+	        while (!cursor.isAfterLast()) {
+	            visitor.apply(cursor)
+	            cursor.moveToNext()
+		    }
+	        cursor.close()
+	    }
+	    
 	}
 	
 	/************************************************************************/
-	
-	/* Migration 1 */
+
+	/* Migration 6 */
 	class CreateDiceSetTable extends Migration {
-	
-		override def up(db: SQLiteDatabase) {
-		    db.execSQL("create table dice_sets (" +
-		    		"_id integer primary key autoincrement, " +
-		    		"name text not null, " +
-		    		"spec text not null," +
-		    		"value text" +
-		    		");")
-		}
-	
-		override def down(db: SQLiteDatabase) {
-		    db.execSQL("drop table dice_sets;")
-		}
-	}
+	    var db: SQLiteDatabase = _
+	    
+	    def tableHasColumn(db: SQLiteDatabase, table: String, column: String): Boolean = {
+	        val cursor = db.rawQuery("pragma table_info (dice_sets)",null)
+	        cursor.moveToFirst()
+	        var found = false
 
-	/* Migration 2 */
-	class CreateNameIndex extends Migration {
-	
-		override def up(db: SQLiteDatabase) {
-		    db.execSQL("delete from dice_sets;")
-		    db.execSQL("create unique index name_index on dice_sets (name)")
-		}
-	
-		override def down(db: SQLiteDatabase) {
-		    db.execSQL("drop index name_index")
-		}
-	}
+	        val checkForColumn = (cursor: Cursor) => {
+	        	if (cursor.getString(1).equals(column)) {
+	        		found = true
+	        	}
+	        }
+	        eachRow(cursor, checkForColumn)
+	        
+	        return found
+	    }
+	    
+	    def isOldVersion(db: SQLiteDatabase): Boolean = tableHasColumn(db, "dice_sets", "dice")
+	        
+	    def convertSpec(old: String): String = {
+	        old.replaceAll("d100", "p10:10+p10:1")
+	    }
+	    
+	    def uniqueName(name: String) = uniqueNameWithSuffix(name, 0)
+	    
+	    def uniqueNameWithSuffix(name: String, suffix: Int) : String = {
+	        val newName = if (suffix == 0) name else name + suffix
+	    	val cursor = db.rawQuery("select name from dice_Sets where name=?", Array(newName))
+	    	val found = cursor.getCount() > 0
+	    	cursor.close()
+	    	return if (found) uniqueNameWithSuffix(name, suffix+1) else newName 
+	    }
+	    
+    	val insertNewRow = (cursor: Cursor) => {
+    		val data = new ContentValues()
+    		data.put("_id", cursor.getInt(0).asInstanceOf[java.lang.Integer])
+    		data.put("name", uniqueName(cursor.getString(1)))
+    		data.put("spec", convertSpec(cursor.getString(2)))
+    		data.putNull("value")
+    		Log.d(TAG, "inserting new dice set " + data.getAsString("name") + ":" + data.getAsString("spec"))
+		    val rowId = db.insert("dice_sets", null, data)
+		    Log.d(TAG, "Inserted " + rowId)
+    	}
 
-	/* Migration 3 */
-	class makeNameNullable extends Migration {
-	
-		override def up(db: SQLiteDatabase) {
-		    db.execSQL("create table new (" +
-		    		"_id integer primary key autoincrement, " +
-		    		"name text, " +
-		    		"spec text not null," +
-		    		"value text" +
-		    		")")
-		    db.execSQL("insert into new select * from dice_sets")
-		    db.execSQL("drop table dice_sets")
-		    db.execSQL("alter table new rename to dice_sets")
+    	override def up(db: SQLiteDatabase) {
+    		this.db = db
+    		val updateOld = isOldVersion(db) 
+		    if (updateOld) {
+		        Log.d(TAG, "renaming old table")
+		    	db.execSQL("alter table dice_sets rename to old_dice_sets")
+		    }
+		    
+		    Log.d(TAG, "creating dice_sets")
+	    	db.execSQL("create table dice_sets (" +
+	    		"_id integer primary key autoincrement, " +
+	    		"name text, " +
+	    		"spec text not null," +
+	    		"value text" +
+	    		")")
 		    db.execSQL("create unique index name_index on dice_sets (name)")
 		    db.execSQL("create index spec_index on dice_sets (spec)")
-		}
-	
-		override def down(db: SQLiteDatabase) {
-		    db.execSQL("drop index name_index")
-		}
+		    		
+		    if (updateOld) {
+		    	Log.d(TAG, "copying old dice to new table")
+		    	val cursor = db.rawQuery("select _id, name, dice from old_dice_Sets", Array[String]())
+		    	eachRow(cursor, insertNewRow)
+		    	Log.d(TAG, "dropping old table")
+		    	db.execSQL("drop table old_dice_sets")
+		    }
+		    Log.d(TAG, "up migration complete")	
+    	}
+    	
+    	override def down(db: SQLiteDatabase) {
+    	    
+    	}
 	}
+	
 }
 
